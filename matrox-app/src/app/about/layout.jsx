3b111faf69b1081fb4bed13d7b4cc620b4205358
Joaquin Rodriguez <kamaju909090@gmail.com>
2026-04-23 18:11:27 -0300

    Agregué cambios en la página About

--- matrox-app/src/app/about/layout.jsx
+++ matrox-app/src/app/about/layout.jsx
@@ -1,32 +1,19 @@
-
 export default function AboutLayout({ children }) {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-2">Sobre Nosotros</h1>
-      <p className="mb-4">
-        Bienvenido a la sección About de la app
-      </p>
+    <div className="w-full min-h-screen bg-[url('/6074593.jpg')] bg-cover bg-center text-white">
+      
+      <div className="p-6 bg-black/50 backdrop-blur-sm">
+        <h1 className="text-3xl font-bold mb-2">Sobre Nosotros</h1>
 
-      <div className="border-t pt-4">
-        {children}
+        <p className="mb-4">
+          Conocemos un poco mejor!
+        </p>
+
+        <div className="pt-4">
+          {children}
+        </div>
       </div>
+
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
